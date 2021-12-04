bed7c9ee389b42248496813104e2671b3f087e1c
nataliaezh <natalya_ezhkova@mail.ru>
2021-12-04 17:02:42 +0300

    домашнее задание урок 2(усложненное)

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-var longNum = 266219;
+var num = 266219;
+var numReorginized = num.toString();
+var multiply = 1;
 
+for (var index = 0; index < numReorginized.length; index++) {
+    multiply = multiply * numReorginized[index];
+}
+console.log(multiply);
 
-console.log(longNum[0] * longNum[1]);
+var multiplyPowed = multiply ** 3;
+console.log("multiplyPowed", multiplyPowed);
+
+var result = multiplyPowed.toString();
+console.log(result.substr(0, 2));
