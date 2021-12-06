2b3540abc58ee9c3b8d74c774b8768294e1d8c3d
nataliaezh <natalya_ezhkova@mail.ru>
2021-12-07 00:19:37 +0300

    домашнее задание к уроку 3

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 var num = 266219;
 var numReorginized = num.toString();
 var multiply = 1;
@@ -12,3 +13,40 @@ console.log("multiplyPowed", multiplyPowed);
 
 var result = multiplyPowed.toString();
 console.log(result.substr(0, 2));
+
+let eng = "en";
+let rus = "ru";
+
+let weekDaysInEnglish =
+    "Sunday, Monday, Tuesday, Wednesday, Thirsday, Friday, Saturday";
+let weekDaysInRussian =
+    "Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье";
+
+let lang = prompt("Выберите язык для вывода названий дней недели (en/ru)");
+
+if (lang == eng) {
+    console.log(weekDaysInEnglish);
+} else if (lang == rus) {
+    console.log(weekDaysInRussian);
+} else {
+    console.log("Ошибка ввода");
+}
+
+switch (lang) {
+    case lang == eng:
+        console.log(weekDaysInEnglish);
+        break;
+    case lang == rus:
+        console.log(weekDaysInRussian);
+        break;
+    default:
+        console.log("Ошибка вввода, повторите попытку");
+}
+
+var days = [
+    [weekDaysInRussian],
+    [weekDaysInEnglish]
+];
+
+let langChoice = lang == eng ? days[(1, 0)] : days[(0, 0)];
+console.log(langChoice);
