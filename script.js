695adaa024f43254454b53d891ae52db7943bf57
nataliaezh <natalya_ezhkova@mail.ru>
2021-12-10 00:02:21 +0300

    усложненное дз к уроку 4

--- script.js
+++ script.js
@@ -1,39 +1,19 @@
-let eng = "en";
-let rus = "ru";
+let input = prompt("Введите любой текст: ");
+let text = "";
+let textLength = 0;
 
-let lang = prompt("Выберите язык для вывода названий дней недели (en/ru)");
-
-if (lang == eng) {
-    console.log(weekDaysInEnglish);
-} else if (lang == rus) {
-    console.log(weekDaysInRussian);
-} else {
-    console.log("Ошибка ввода");
-}
-
-switch (lang) {
-    case lang == eng:
-        console.log(weekDaysInEnglish);
-        break;
-    case lang == rus:
-        console.log(weekDaysInRussian);
-        break;
-    default:
-        console.log("Ошибка вввода, повторите попытку");
-}
-
-let days = [];
-days["ru"] = ["Пн", "Вт", "СР", "Чт", "Пт", "Сб", "Вс"];
-days["en"] = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-
-console.log(days[lang]);
-
-let namePerson = prompt("Введите имя: ");
-
-let answer =
-    namePerson == "Александр" ?
-    "Преподаватель" :
-    "Артем" ?
-    "Директор" :
-    "студент";
-console.log(answer);
+const manageInput = function(input) {
+    if (typeof input != "string") {
+        alert("Ошибка ввода, введите текст");
+    } else {
+        text = input.trim();
+    }
+    textLength = text.length;
+    if (textLength > 30) {
+        text = text.slice(0, 29) + "...";
+        console.log(text);
+    } else {
+        console.log(text);
+    }
+};
+console.log(manageInput(input));
